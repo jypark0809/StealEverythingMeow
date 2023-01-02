23ce8f1fa7828a575570d6b328813ee34e69a9f1
oyounghawng <108117012+oyounghawng@users.noreply.github.com>
2023-01-03 02:36:07 +0900

    RoomOpen

--- Assets/Scripts/LobbySceneScript/GameManagerLobby.cs
+++ Assets/Scripts/LobbySceneScript/GameManagerLobby.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class GameManagerLobby : MonoBehaviour
 {
     public GameObject[] Room;
-    public UImanager_Lobby theUi;
     Camera Camera;
     public int current_room = 0;
 
@@ -13,30 +12,23 @@ public class GameManagerLobby : MonoBehaviour
     private void Start()
     {
         Camera = GetComponent<Camera>();
-        theUi = FindObjectOfType<UImanager_Lobby>();
     }
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
             Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             Debug.DrawRay(mousepos, ray.direction * 100.0f, Color.red, 1.0f);
+
             if (Physics.Raycast(ray, out hit, LayerMask.GetMask("Cat")))
             {
-                //theUi.RoomOpenPanel();
-                Debug.Log(hit.collider.gameObject.name);
-                hit.collider.gameObject.transform.position = new Vector3(mousepos.x,mousepos.y,0);
+                Debug.Log("asd");
+                Managers.Resource.Instantiate("UI/Popup/Room_UI");
+                //hit.collider.gameObject.transform.position = new Vector3(mousepos.x,mousepos.y,0);
             }
         }
 
     }
-
-    public void RoomOpen()
-    {
-        Room[current_room].SetActive(false);
-        current_room++;
-    }
-
 }
--- Assets/Scripts/LobbySceneScript/UI/Popup/StoreUI.cs
+++ Assets/Scripts/LobbySceneScript/UI/Popup/StoreUI.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StorePanel : MonoBehaviour
+[System.Serializable]
+public class Craft
+{
+    public string craftName;
+    public GameObject go_prefab;
+    public GameObject go_previwPrefab;
+}
+
+
+public class StoreUI : UI_Popup
 {
     public List<TabButton> tabButtons;
     public List<GameObject> contentsPanels;
 
+    [SerializeField]
+    private Craft[] somsom;
+
+    private GameObject go_Previw;
 
     public void ClickTab(int id)
     {
@@ -23,5 +36,12 @@ public class StorePanel : MonoBehaviour
                 tabButtons[i].DeSeleted();
             }
         }
+    }
+
+    public void BuySOM()
+    {
+        go_Previw = Instantiate(somsom[0].go_previwPrefab, new Vector3(0.5f, 0.5f, 0), Quaternion.identity);
+        go_Previw.transform.parent = FindObjectOfType<Grid>().transform;
+        Destroy(this.gameObject);
     }
 }
