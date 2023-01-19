b78e08e39fc9dd09c8c20205f9832ba64e35cb46
oyounghawng <108117012+oyounghawng@users.noreply.github.com>
2023-01-19 17:04:30 +0900

    store

--- Assets/Scripts/LobbySceneScript/SomSom.cs
+++ Assets/Scripts/LobbySceneScript/SomSom.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SomSom : MonoBehaviour
 {
     public bool IsRoomOpen;
-    public bool IsUpgrdae;
+    public bool IsUpgrdae = false;
     private void Start()
     {
         //StartCoroutine("RoomOpen"); // 룸 확장조건 여부체크
@@ -14,7 +14,10 @@ public class SomSom : MonoBehaviour
     private void OnMouseDown()
     {
         if (IsRoomOpen)
+        {
             Managers.UI.ShowPopupUI<UI_UnlockRoomPopup>();
+            IsRoomOpen = false;
+        }
         else if (IsUpgrdae)
             Managers.UI.ShowPopupUI<UI_UpgradeSom>();
 
--- Assets/Scripts/UI/Popup/UI_Store.cs
+++ Assets/Scripts/UI/Popup/UI_Store.cs
@@ -27,13 +27,18 @@ public class UI_Store : UI_Popup
         GetButton((int)Buttons.BuySomsomButton).gameObject.BindEvent(BuySomsomButton);
         GetButton((int)Buttons.CloseButton).gameObject.BindEvent(OnCloseButton);
         GetButton((int)Buttons.BuyFurnitureButton).gameObject.BindEvent(BuyFurnitureButton);
+
+        if(Managers.Game.SaveData.currentFurniture ==5)
+            GetButton((int)Buttons.BuyFurnitureButton).interactable = false;
     }
 
     void BuySomsomButton(PointerEventData evt)
     {
+        Managers.Resource.Destroy(Util.FindChild(Managers.Object.CatHouse.gameObject, "newspaper", true));
         GameObject go = Managers.Resource.Instantiate("Somsom", Managers.Object.CatHouse.transform);
         go.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "Som_Spwan_Point").transform.position;
         Managers.UI.ClosePopupUI();
+
     }
     void BuyFurnitureButton(PointerEventData evt)
     {
@@ -41,28 +46,24 @@ public class UI_Store : UI_Popup
         switch (Managers.Game.SaveData.currentFurniture)
         {
             case 0:
-                GameObject go0 = Managers.Resource.Instantiate("0", Managers.Object.CatHouse.transform);
-                go0.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "0", true).transform.position;
+                GameObject go0 = Managers.Resource.Instantiate("1", Managers.Object.CatHouse.transform);
+                go0.transform.position = (Util.FindChild(Managers.Object.CatHouse.gameObject, "1", true)).transform.position;
                 break;
             case 1:
-                GameObject go1 = Managers.Resource.Instantiate("1", Managers.Object.CatHouse.transform);
-                go1.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "1", true).transform.position;
+                GameObject go1 = Managers.Resource.Instantiate("2", Managers.Object.CatHouse.transform);
+                go1.transform.position = (Util.FindChild(Managers.Object.CatHouse.gameObject, "2", true)).transform.position;
                 break;
             case 2:
-                GameObject go2 = Managers.Resource.Instantiate("2", Managers.Object.CatHouse.transform);
-                go2.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "2", true).transform.position;
+                GameObject go2 = Managers.Resource.Instantiate("3", Managers.Object.CatHouse.transform);
+                go2.transform.position = (Util.FindChild(Managers.Object.CatHouse.gameObject, "3", true)).transform.position;
                 break;
             case 3:
-                GameObject go3 = Managers.Resource.Instantiate("3", Managers.Object.CatHouse.transform);
-                go3.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "3", true).transform.position;
+                GameObject go3 = Managers.Resource.Instantiate("4", Managers.Object.CatHouse.transform);
+                go3.transform.position = (Util.FindChild(Managers.Object.CatHouse.gameObject, "4", true)).transform.position;
                 break;
             case 4:
-                GameObject go4 = Managers.Resource.Instantiate("4", Managers.Object.CatHouse.transform);
-                go4.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "4", true).transform.position;
-                break;
-            case 5:
-                GameObject go5 = Managers.Resource.Instantiate("5", Managers.Object.CatHouse.transform);
-                go5.transform.position = Util.FindChild(Managers.Object.CatHouse.gameObject, "5", true).transform.position;
+                GameObject go4 = Managers.Resource.Instantiate("5", Managers.Object.CatHouse.transform);
+                go4.transform.position = (Util.FindChild(Managers.Object.CatHouse.gameObject, "5", true)).transform.position;
                 break;
         }
         Managers.Game.SaveData.currentFurniture++;
