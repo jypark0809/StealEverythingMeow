46166a77baf830e45f6412168a65ca49f0da6318
jypark0809 <jypark_0809@naver.com>
2023-02-08 23:03:44 +0900

    UI 밑에 있는 오브젝트 터치 버그 수정

--- Assets/Scripts/UI/Popup/UI_ConversationScript.cs
+++ Assets/Scripts/UI/Popup/UI_ConversationScript.cs
@@ -163,7 +163,7 @@ public class UI_ConversationScript : UI_Popup
     {
         List<Script> scripts = new List<Script>();
         scripts.Add(new Script("전 주인", true, 0, "아기 고양이일 때는 예뻤는데\n갈수록 못생겨지네..\n슬슬 새로운 애를 데려와 볼까?"));
-        scripts.Add(new Script("", false, 1, "어느 추운 겨울날,\n\n차도에 버려진 하양이는 떠나간\n차만 하염없이 바라보는데..."));
+        scripts.Add(new Script("", false, 1, "어느 추운 겨울날,\n\n차도에 버려진 하양이는 \n떠나간 차만 하염없이 바라보는데..."));
         scripts.Add(new Script("하양이", true, 2, "춥고.. 배고파..\n나 이제 어떻게 살지...?"));
         scripts.Add(new Script("", false, 3, "인적이 드문 뒷산 인근,\n\n텅 빈 집을 발견한 하양이"));
         scripts.Add(new Script("하양이", true, 3, "이 집도 나처럼 버려진 집인가..?\n여기서 제 2의 묘생을 시작하는 거야!"));
@@ -171,7 +171,7 @@ public class UI_ConversationScript : UI_Popup
         scripts.Add(new Script("하양이", true, 4, "너희들도 나와 같은 처지인 거야?\n나랑 함께 할래?"));
         scripts.Add(new Script("하양이", true, 5, "두고 보자... 다 훔치고\n부숴버릴 테다...\n시작은 이 집이야!"));
         scripts.Add(new Script("", false, 5, "고양이들은 먹고 살기 위해,\n\n버려진 지난 날의 복수를 위해,\n\n인간들의 집을 털기로 합니다."));
-        scripts.Add(new Script("", false, 5, "오늘부터 공범이야, 우리...\n\n<다훔칠고양>"));
+        scripts.Add(new Script("", false, 5, "오늘부터 공범이야, 우리\n\n<다훔칠고양>"));
 
         return scripts;
     }    
